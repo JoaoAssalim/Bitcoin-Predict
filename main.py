dd0a789867f51514a4e7f85f0b597da21992b351
João Assalim <joassalim@gmail.com>
2023-08-16 23:25:26 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from model import model
 from model_page import model_page
 from predict_page import predict_page
 from information import info
-import webbrowser
 
 def main():
     st.set_page_config(page_title="Bitcoin Predict Price", page_icon=None, layout="centered", initial_sidebar_state="auto", menu_items=None)
@@ -11,9 +10,6 @@ def main():
         "Chosse the page",
         ("Project Informations", "View model", "Try prediction")
     )
-
-    if st.sidebar.button("Repository", use_container_width=True):
-        webbrowser.open_new_tab("https://github.com/JoaoAssalim/Bitcoin-Predict")
 
     if menu == "View model":
         model_page()
